a9129a2856384bae525b939f1ecac0148b0c633f
kelvin john <kelvin12299@gmail.com>
2026-02-27 15:11:59 +0200

    inital: Seminar 2 for java course - Student class and  counter

---
+++ src/seminar2/Country.java
@@ -0,0 +1,6 @@
+package seminar2;
+
+public enum Country {
+	latvia, Germany,Nigeria,greece,spain
+
+}
---
+++ src/seminar2/Student.java
@@ -0,0 +1,34 @@
+package seminar2;
+
+public class Student {
+	private String matriculaNumber;
+	private String name;
+	private String surname;
+	private String faculty;
+	private int birthYear;
+	private Country country;
+	private String passportNumber;
+	
+	public String getMatriculation() {
+		return matriculaNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getFaculty() {
+		return faculty;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public String getPassportNumber() {
+		return passportNumber;
+	}
+
+}
